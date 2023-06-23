03a11f4a9d85e1eaf9f28f63c073853b2a9788b3
Ivan Diaz <ivdiazsa@microsoft.com>
2023-06-23 14:15:15 -0700

    Added all my emacs.d files.

---
+++ emacs.d/elpa/flatui-dark-theme-0.3.0/flatui-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from flatui-dark-theme.el  -*- no-byte-compile: t -*-
+(define-package "flatui-dark-theme" "0.3.0" "Dark color theme with colors from https://flatuicolors.com/" '((emacs "24")) :commit "af5c84e2a2810748cc71a68ec7ba333097cc1f63" :authors '(("Andrew Phillips" . "theasp@gmail.com")) :maintainer '("Andrew Phillips" . "theasp@gmail.com") :keywords '("color" "theme" "dark" "flatui" "faces") :url "https://github.com/theasp/flatui-dark-theme")
