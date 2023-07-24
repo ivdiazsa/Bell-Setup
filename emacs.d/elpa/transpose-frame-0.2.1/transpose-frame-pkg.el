811eaf6161359136ee52bbbdfbf1f0be79de154c
Ivan Diaz <ivdiazsa@microsoft.com>
2023-07-24 12:10:47 -0700

    Added transpose-frame package installation to emacs.

---
+++ emacs.d/elpa/transpose-frame-0.2.1/transpose-frame-pkg.el
@@ -0,0 +1,11 @@
+(define-package "transpose-frame" "0.2.1" "Transpose windows arrangement in a frame" 'nil :commit "7b7f8a1582436749a57ebbba6ead716b5a0edddc" :authors
+  '(("S. Irie"))
+  :maintainers
+  '(("S. Irie"))
+  :maintainer
+  '("S. Irie")
+  :keywords
+  '("window"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
