4642f2da2c35dab58c8d6c2fa21c4b6fd2c0c038
Ivan Diaz <ivdiazsa@microsoft.com>
2023-08-03 16:16:17 -0700

    Added Vimrc/Vimscript mode to Emacs.

---
+++ emacs-files/emacs.d/elpa/vimrc-mode-0.3.1/vimrc-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from vimrc-mode.el  -*- no-byte-compile: t -*-
+(define-package "vimrc-mode" "0.3.1" "Major mode for vimrc files" 'nil :keywords '("languages" "vim") :url "https://github.com/mcandre/vimrc-mode")
