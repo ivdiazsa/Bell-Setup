94ac14587b47ef00d433a014af3e98a41afc29d3
Ivan Diaz <ivdiazsa@microsoft.com>
2023-09-13 17:13:37 -0700

    Began working on a little tool to port themes between Emacs, Vim, and VS Code.

---
+++ ThemeConverter/ThemeConverter.cs
@@ -0,0 +1,21 @@
+// Little Tool to convert between Emacs, Vim, and VS Code themes.
+using System;
+using System.Text;
+
+public class ThemeConverter
+{
+    private const string EMACS_THEME_COMMENT_HEADER = ";;; Theme Code:";
+
+    static int Main(string[] args)
+    {
+        // Validate we actually receive a theme source file to convert.
+        string srcThemeFile = args[0];
+
+        // Validate we actually receive which editor to convert the theme file to.
+        string destEditor = args[1];
+
+        var outputThemeCode = new StringBuilder();
+        outputThemeCode.AppendLine(EMACS_THEME_COMMENT_HEADER);
+        return 0;
+    }
+}
