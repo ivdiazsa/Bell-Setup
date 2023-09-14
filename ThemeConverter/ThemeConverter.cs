e4dcbcdb97f30146f6b124b1f23c4664f9818aee
Ivan Diaz <ivdiazsa@microsoft.com>
2023-09-13 17:16:04 -0700

    Forgot to add a little note.

--- ThemeConverter/ThemeConverter.cs
+++ ThemeConverter/ThemeConverter.cs
@@ -15,7 +15,10 @@ public class ThemeConverter
         string destEditor = args[1];
 
         var outputThemeCode = new StringBuilder();
+
+        // Use the corresponding header to the target editor provided.
         outputThemeCode.AppendLine(EMACS_THEME_COMMENT_HEADER);
+
         return 0;
     }
 }
