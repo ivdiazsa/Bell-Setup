58b065f18eb7d679dfb7812a8fdd5cd0851a9e97
Ivan Diaz <ivdiazsa@microsoft.com>
2023-09-20 15:28:47 -0700

    Added dot-mode and multiple-cursors to emacs, updated the Emacs configuration file, and removed the my-todos file since it's changing too often.

---
+++ emacs-files/emacs.d/elpa/dot-mode-1.13/dot-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from dot-mode-1.13/dot-mode.el  -*- no-byte-compile: t -*-
+(define-package "dot-mode" "1.13" "minor mode to repeat typing or commands" '((emacs "24.3")) :authors '(("Robert Wyrick" . "rob@wyrick.org")) :maintainer '("Robert Wyrick" . "rob@wyrick.org") :keywords '("convenience") :url "https://github.com/wyrickre/dot-mode")
