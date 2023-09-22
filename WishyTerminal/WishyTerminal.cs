1523ee685ff595dbdd0ab490ee26ad43a4299c04
Ivan Diaz <ivdiazsa@microsoft.com>
2023-09-21 21:28:32 -0700

    Implemented the basic REPL for my Wishy Terminal and updated the .gitignore file.

---
+++ WishyTerminal/WishyTerminal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+public class WishyTerminal
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine("\nWelcome to Wishy Terminal!\n");
+
+        ConsoleKeyInfo input;
+        string command = string.Empty;
+        StringBuilder commandSb = new StringBuilder();
+
+        // Main interactive loop of the shell!
+        while (true)
+        {
+            commandSb.Clear();
+            Console.Write("\nPrompt goes here::> ");
+
+            do
+            {
+                input = Console.ReadKey();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.Enter:
+                        break;
+
+                    default:
+                        commandSb.Append(input.KeyChar);
+                        break;
+                }
+            }
+            while (input.Key != ConsoleKey.Enter);
+
+            command = commandSb.ToString();
+            Console.Write("\n");
+
+            // Command processing goes here!
+            Console.WriteLine("You typed the command {0}!", command);
+
+            if (command == "exit")
+                break;
+        }
+
+        Console.WriteLine("\nWishy Terminal wishes you a wonderful day!"
+                          + " Thanks for your patronage!");
+        return 0;
+    }
+}
