ba731a975d018e644473940c60146aa89fc18aa7
Ivan Diaz <ivdiazsa@microsoft.com>
2023-09-21 20:54:41 -0700

    Implemented the Console Menu in the Code Tester.'

--- CodeTester/CodeTester.cs
+++ CodeTester/CodeTester.cs
@@ -5,33 +5,117 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+// Currently used to display the test messages for a few seconds.
+using System.Threading;
 
 [AttributeUsage(AttributeTargets.Method)]
 internal class TestableAttribute : Attribute { }
 
 public static class CodeTester
 {
+    // static int Main(string[] args)
+    // {
+    //     BindingFlags methodSearchFlags = BindingFlags.Public
+    //                                    | BindingFlags.NonPublic
+    //                                    | BindingFlags.Static;
+
+    //     MethodInfo[] methods = typeof(CodeTester).GetMethods(methodSearchFlags)
+    //                                              .Where(IsTestableMethod)
+    //                                              .ToArray();
+
+    //     foreach (MethodInfo mi in methods)
+    //     {
+    //         Console.WriteLine("* {0}", mi.ToString());
+    //     }
+    //     Console.Write("\n");
+
+    //     foreach (MethodInfo mi in methods)
+    //     {
+    //         mi.Invoke(null, null);
+    //     }
+
+    //     return 0;
+    // }
+
     static int Main(string[] args)
     {
-        BindingFlags methodSearchFlags = BindingFlags.Public
-                                       | BindingFlags.NonPublic
-                                       | BindingFlags.Static;
+        string[] options = { "Option1", "Option2", "Exit" };
+        ConsoleKeyInfo input;
+        string optionPrefix = "  ";
 
-        MethodInfo[] methods = typeof(CodeTester).GetMethods(methodSearchFlags)
-                                                 .Where(IsTestableMethod)
-                                                 .ToArray();
+        int optionIndex = 0;
+        int selectedOption = 0;
+        bool exitSelected = false;
 
-        foreach (MethodInfo mi in methods)
+        Console.CursorVisible = false;
+
+        // Console and menu management.
+        do
         {
-            Console.WriteLine("* {0}", mi.ToString());
-        }
-        Console.Write("\n");
+            // Exit if the "Exit" option was selected.
+            if (exitSelected)
+                break;
 
-        foreach (MethodInfo mi in methods)
-        {
-            mi.Invoke(null, null);
-        }
+            // Clear the console for the next render.
+            Console.Clear();
 
+            // Print all the options with the currently selected one colored.
+            for (optionIndex = 0; optionIndex < 3; optionIndex++)
+            {
+                if (selectedOption == optionIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    optionPrefix = "* ";
+                }
+                else
+                {
+                    optionPrefix = "  ";
+                }
+
+                Console.WriteLine("{0}{1}", optionPrefix, options[optionIndex]);
+                Console.ResetColor();
+            }
+
+            input = Console.ReadKey(false);
+            switch (input.Key)
+            {
+                case ConsoleKey.DownArrow:
+                    selectedOption = ++selectedOption % 3;
+                    break;
+
+                case ConsoleKey.UpArrow:
+                    selectedOption = selectedOption > 0 ? --selectedOption
+                                                        : selectedOption = 3 - 1;
+                    break;
+
+                case ConsoleKey.Enter:
+                    // Handle the selected option and act accordingly :)
+                    if (selectedOption == 0)
+                    {
+                        Console.WriteLine("\nYou selected Option 1!\n");
+                        Thread.Sleep(2000);
+                    }
+                    else if (selectedOption == 1)
+                    {
+                        Console.WriteLine("\nYou selected Option 2!\n");
+                        Thread.Sleep(2000);
+                    }
+                    else
+                    {
+                        exitSelected = true;
+                        Console.WriteLine("\nThanks for participating!\n");
+                        Thread.Sleep(2000);
+                    }
+                    break;
+
+                default:
+                    Console.Beep();
+                    break;
+            }
+        }
+        while (input.Key != ConsoleKey.Escape);
+
+        Console.WriteLine("Thanks for stopping by!");
         return 0;
     }
 
