bb8a12635df8d3b58baad36f0444f2cd93165d0c
Ivan Diaz <ivdiazsa@microsoft.com>
2023-10-12 16:22:08 -0700

    Added some new shortcuts to the Emacs configuration file, as well as enabled by default the mode to replace selected text when typing or pasting over it. Also, added two new little files to make those two themes cover the terminal as well :)

---
+++ emacs-files/emacs-cyberpunk-term-colors.el
@@ -0,0 +1,18 @@
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(term-color-black ((t (:foreground "#555753"))))
+ '(term-color-blue ((t (:foreground "#7B68EE"))))
+ '(term-color-cyan ((t (:foreground "#AFD8AF"))))
+ '(term-color-green ((t (:foreground "#DC8CC3"))))
+ '(term-color-magenta ((t (:foreground "#CD1076"))))
+ '(term-color-red ((t (:foreground "#8B0000"))))
+ '(term-color-white ((t (:foreground "#EEEEEC"))))
+ '(term-color-yellow ((t (:foreground "#D0BF8F"))))
+ '(term-default-bg-color ((t (:inherit term-color-black))))
+ '(term-default-fg-color ((t (:inherit term-color-white)))))
---
+++ emacs-files/emacs-vscode-term-colors.el
@@ -0,0 +1,18 @@
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(term-color-black ((t (:foreground "#1E1E1E"))))
+ '(term-color-blue ((t (:foreground "#9CDCFE"))))
+ '(term-color-cyan ((t (:foreground "#6A9955"))))
+ '(term-color-green ((t (:foreground "#4EC9B0"))))
+ '(term-color-magenta ((t (:foreground "#C586C0"))))
+ '(term-color-red ((t (:foreground "#D16969"))))
+ '(term-color-white ((t (:foreground "#D4D4D4"))))
+ '(term-color-yellow ((t (:foreground "#DCDCAA"))))
+ '(term-default-bg-color ((t (:inherit term-color-black))))
+ '(term-default-fg-color ((t (:inherit term-color-white)))))
