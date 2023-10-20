79471e6ec5f2554d1c0f1e68edfe0f2f341a8992
Ivan Diaz <ivdiazsa@microsoft.com>
2023-10-20 11:46:14 -0700

    Added some new convenient configuration for Vim, and a bit of customized setup for Nvim-Tree and Neogit for Neovim.

--- vim-files/neovim/load-nvim-plugins.lua
+++ vim-files/neovim/load-nvim-plugins.lua
@@ -9,25 +9,24 @@ vim.g.loaded_netrwPlugin = 1
 -- Set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- Empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
--- require("nvim-tree").setup({
---   sort_by = "case_sensitive",
---   view = {
---     width = 30,
---   },
---   renderer = {
---     group_empty = true,
---   },
---   filters = {
---     dotfiles = true,
---   },
--- })
+-- Customized setup!
+require("nvim-tree").setup({
+    actions =
+    {
+        open_file =
+        {
+            resize_window = false
+        }
+    }
+})
 
 -- ***************************
 -- * Neogit Configuration!
 -- ***************************
-require("neogit").setup()
+
+require("neogit").setup({
+    disable_insert_on_commit = "auto",
+    auto_refresh = false,
+    kind = "vsplit",
+})
 
