22f95d4eb6f025f7ffc68ee5b555daa61f30c59b
Ivan Diaz <ivdiazsa@microsoft.com>
2023-10-22 10:50:55 -0700

    Full update of Emacs packages and the emacs configuration file.

---
+++ emacs-files/emacs.d/elpa/github-dark-vscode-theme-20231011.2034/github-dark-vscode-theme-pkg.el
@@ -0,0 +1,14 @@
+(define-package "github-dark-vscode-theme" "20231011.2034" "The GitHub Dark Theme from Visual Studio Code"
+  '((emacs "24.1"))
+  :commit "9906e1412d692ea0c115998c1809bcc135e263f6" :authors
+  '(("Justintime50"))
+  :maintainers
+  '(("Justintime50"))
+  :maintainer
+  '("Justintime50")
+  :keywords
+  '("faces")
+  :url "https://github.com/justintime50/github-dark-vscode-emacs-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs-files/emacs.d/elpa/vs-light-theme-20230929.2331/vs-light-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vs-light-theme" "20230929.2331" "Visual Studio IDE light theme"
+  '((emacs "24.1"))
+  :commit "ddee29725dd7e35e97855c7b5c65d3e119498a1a" :authors
+  '(("Jen-Chieh Shen"))
+  :maintainers
+  '(("Jen-Chieh Shen"))
+  :maintainer
+  '("Jen-Chieh Shen")
+  :url "https://github.com/emacs-vs/vs-light-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
