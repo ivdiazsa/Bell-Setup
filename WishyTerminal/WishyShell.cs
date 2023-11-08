a9b07c54bc3b09e889176c6f69e7cbcdb5245ead
Ivan Diaz <ivdiazsa@microsoft.com>
2023-11-07 16:02:13 -0800

    Began implementing the command-line argument parsing.

--- WishyTerminal/WishyShell.cs
+++ WishyTerminal/WishyShell.cs
@@ -6,8 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-using WishyExtensions;
-
 internal static class WishyShell
 {
     // Int ExecuteCommand():
@@ -30,6 +28,31 @@ internal static class WishyShell
 
     private static (string, string[]) ParseCommandLineArgs(string input)
     {
-        return (string.Empty, Array.Empty<string>());
+        // Clean up any accidental leading or trailing whitespace. We have to do this
+        // step because spaces can have a special meaning in certain commands, so we
+        // don't want to ignore them altogether.
+
+        input = input.Trim();
+
+        // The parameter list starts at the first space.
+        int argsStart = input.IndexOf(' ');
+
+        // If the string we receive has no spaces, then we can safely assume the
+        // command was called with no arguments, so we just return it.
+
+        if (argsStart < 0)
+            return (input, Array.Empty<string>());
+
+        string command = input.Substring(0, argsStart);
+        List<string> parsedArgs = new List<string>();
+
+        char quote = '\"';
+        bool inQuotes = false;
+
+        for (int i = argsStart; i < input.Length; i++)
+        {
+        }
+
+        return (command, parsedArgs.ToArray());
     }
 }
