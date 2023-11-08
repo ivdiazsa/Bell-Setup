38e1d0f76df4614c09f59c6eb106a5f7701d3a2b
Ivan Diaz <ivdiazsa@microsoft.com>
2023-11-08 14:58:39 -0800

    Wishy Terminal command-line processing is functional! Mostly... Right now, it processes them incorrectly if there are contiguous spaces outside of quotes.

--- WishyTerminal/WishyShell.cs
+++ WishyTerminal/WishyShell.cs
@@ -16,6 +16,10 @@ internal static class WishyShell
     public static int ExecuteCommand(string input)
     {
         // Here we parse the input and call the command that was called with it.
+        (string command, string[] args) = ParseCommandLineArgs(input);
+
+        Console.WriteLine("\nCOMMAND: {0}", command);
+        Console.WriteLine("ARGS: {0}", string.Join('-', args));
         return 0;
     }
 
@@ -31,7 +35,6 @@ internal static class WishyShell
         // Clean up any accidental leading or trailing whitespace. We have to do this
         // step because spaces can have a special meaning in certain commands, so we
         // don't want to ignore them altogether.
-
         input = input.Trim();
 
         // The parameter list starts at the first space.
@@ -39,7 +42,6 @@ internal static class WishyShell
 
         // If the string we receive has no spaces, then we can safely assume the
         // command was called with no arguments, so we just return it.
-
         if (argsStart < 0)
             return (input, Array.Empty<string>());
 
@@ -48,11 +50,55 @@ internal static class WishyShell
 
         char quote = '\"';
         bool inQuotes = false;
+        StringBuilder nextArgSb = new StringBuilder();
 
-        for (int i = argsStart; i < input.Length; i++)
+        // We start here with argsStart + 1 because that space is not part of the
+        // parameters list, just the separator from the command.
+        for (int i = argsStart+1; i < input.Length; i++)
         {
-        }
+            char c = input[i];
 
+            // Handling quoted parameters: If we find the closing quote, then we have
+            // finished parsing this specific parameter. Add the contents of the
+            // arg StringBuilder to the list of parsed args and clear it.
+            if (inQuotes)
+            {
+                nextArgSb.Append(c);
+
+                if (c == quote)
+                {
+                    parsedArgs.Add(nextArgSb.ToString());
+                    nextArgSb.Clear();
+                    inQuotes = false;
+                }
+                continue;
+            }
+
+            if (c == ' ')
+            {
+                // We found an extra space.
+                if (nextArgSb.Length == 0)
+                    continue;
+
+                // A space in a non-quoted parameter is the delimiter. So, we add
+                // the argument to the list and clear the StringBuilder.
+                parsedArgs.Add(nextArgSb.ToString());
+                nextArgSb.Clear();
+            }
+            else if (c == '\'' || c == '\"')
+            {
+                // Found an opening quote.
+                quote = c;
+                inQuotes = true;
+                nextArgSb.Append(c);
+            }
+            else
+            {
+                // The most normal scenario we can encounter: Normal character in
+                // an unquoted string.
+                nextArgSb.Append(c);
+            }
+        }
         return (command, parsedArgs.ToArray());
     }
 }
