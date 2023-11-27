695a8a713a03ddf6fb06cf87826ca783f09acd38
Ivan Diaz <ivdiazsa@microsoft.com>
2023-11-27 15:47:06 -0800

    More progress with theme-converter...

--- theme-converter/vscode-to-emacs-converter.rb
+++ theme-converter/vscode-to-emacs-converter.rb
@@ -5,31 +5,7 @@
 
 require 'json'
 
-# Little application's helper functions and objects!
-# This class generates the initial (defcustom) calls of the Elisp file.
-
-module DefcustomMethod
-
-  def self.get_defcustom_header(theme_name, custom_name)
-    return "defcustom #{theme_name}-#{custom_name} t"
-  end
-
-  def self.get_defcustom_doc(custom_name)
-    return "Set box around TODO items in org-mode." if custom_name == 'box-org-todo'
-    return "Scale headlines and other org faces." if custom_name == 'scale-org-faces'
-    return "Set (:invert-video t) on hl-todo face." if custom_name == 'invert-hl-todo'
-    raise "get_defcustom_doc: Value #{custom_name} not yet implemented."
-  end
-
-  def self.get_defcustom_type(type)
-    return ":type #{type}"
-  end
-
-  def self.get_defcustom_group(group)
-    return ":group #{group}"
-  end
-
-end
+require './defcustommethod'
 
 # Class that serves as the main component of our little app :)
 
@@ -79,6 +55,9 @@ class ThemeConverter
       @elisp_translated_code << gen_defcustom(name)
     end
 
+    # Generate the big list of colors at the beginning of the (let) statement.
+    @elisp_translated_code << gen_colors_letlist(theme_colors, token_colors)
+
     # Write down all our code to the new theme file!
     File.open(@elisp_file, 'w') do |ef|
       ef.write(@elisp_translated_code)
@@ -101,9 +80,15 @@ class ThemeConverter
     code <<  "  #{DefcustomMethod.get_defcustom_group("'dark-plus")})\n"
     return code
   end
+
+  def gen_colors_letlist(theme_c. tokens_c)
+    code = "\n(let ((class '((class color) (min-colors 89)))\n"
+  end
 end
 
+# ******************************************* #
 # Little application script starting point :)
+# ******************************************* #
 
 # ENHANCEME: Validate we actually received at least one parameter value.
 vscode_file = ARGV[0]
