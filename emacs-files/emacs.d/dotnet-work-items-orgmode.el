a2b8ed36d32ac0af36f44b1e5e605f53dcce550e
Ivan Diaz <ivdiazsa@microsoft.com>
2024-01-03 13:52:10 -0800

    Finished implementing the org-mode configuration for my job's dotnet work items.

--- emacs-files/emacs.d/dotnet-work-items-orgmode.el
+++ emacs-files/emacs.d/dotnet-work-items-orgmode.el
@@ -12,11 +12,52 @@
 (setq org-lowest-priority 3)
 (setq org-default-priority 2)
 
+;; **********************************
+;;  Work Item Information Functions!
+;; **********************************
+
+(defun get-work-item-checklist ()
+  "Prompt for the initial planned subtasks for the current work item."
+  (setq checklist "")
+  (let ((continue (read-answer "Do you wish to add checklist items? "
+                               '(("yes" nil "add the list of tasks")
+                                 ("no"  nil "finish capturing this work item")))))
+
+    (while (equal continue "yes")
+      (let* ((task-name (read-string "Enter the next task: "))
+             (has-subtasks (read-answer "Does this task have further subtasks? "
+                                        '(("yes" nil "add the list of subtasks")
+                                          ("no"  nil "continue adding tasks"))))
+             (task-item (format "\n- [ ] %s" task-name)))
+
+        (if (equal has-subtasks "yes")
+            (setq checklist (concat checklist (format "%s [%%]\n" task-item)))
+          (setq checklist (concat checklist (format "%s\n" task-item))))
+
+        (while (equal has-subtasks "yes")
+          (let* ((subtask-name (read-string "Enter the subtask: "))
+                 (subtask-item (format "  - [ ] %s\n" subtask-name)))
+
+            (setq checklist (concat checklist subtask-item)))
+
+          (setq has-subtasks
+                (read-answer "Add another subtask? "
+                             '(("yes" nil "prompt for the next subtask")
+                               ("no"  nil "end and move to the next task"))))))
+
+        (setq continue (read-answer "Add another task? "
+                                    '(("yes" nil "prompt for the next task")
+                                      ("no"  nil "finish capturing this work item"))))))
+
+  (if (string-equal checklist "")
+      (format "- [ ] Do work item\n")
+    (format "%s" checklist)))
+
 ;; *********************
 ;;  New Item Templates!
 ;; *********************
 
-;; Since org-mode is for everyone, let's add the japanese dictionary capture
+;; Since org-mode is for everyone, let's add the dotnet new work item capture
 ;; templates to emacs' "org-capture-templates", rather than setting it from
 ;; scratch. We don't want to delete the templates from other org-mode components
 ;; we might have initialized prior to this one :)
@@ -24,14 +65,15 @@
 (add-to-list 'org-capture-templates
              '("i" "New Work Item"
                entry (file+headline dotnet-work-todos-file "Work Items")
-               "** NEW ITEM [#2] %^{Job Item Title}\n\
+               "** NEW ITEM [#2] %^{Job Item Title} [%]\n\
 %^g\n\
-:Created: %<%Y/%m/%e %l:%M %P %Z>\n\n\
+:Created: %<%Y/%m/%d %l:%M %P %Z>\n\n\
 %^{Job Item Description}\n\n\
 *- Repository: %^{Repository Name (what comes after github.com/)}*\n\
 *- Issue Number: %^{Issue Number}*\n\
-*- Link: [[https://github.com/%\\3/issues/%\\4]]*\n\n"
-               :empty-lines 1 :immediate-finish :jump-to-captured)
+*- Link: [[https://github.com/%\\3/issues/%\\4]]*\n\
+%(get-work-item-checklist)\n"
+               :empty-lines 1 :immediate-finish t :jump-to-captured t)
              t)
 
 ;; **************
@@ -39,30 +81,30 @@
 ;; **************
 
 (setq org-todo-keywords
-      '((sequence "New Item"
-                  "In Progress(!)"
-                  "Researching(@/!)"
-                  "Backlogged(@/!)"
-                  "In Review(!)"
-                  "Blocked(@/!)"
-                  "Warning(@/!)"
-                  "Complete(!)"
-                  "Discarded(@/!)")))
+      '((sequence "NEW ITEM(n)"
+                  "IN PROGRESS(i!)"
+                  "RESEARCHING(r@/!)"
+                  "BACKLOGGED(l@/!)"
+                  "IN REVIEW(r!)"
+                  "BLOCKED(b@/!)"
+                  "WARNING(w@/!)"
+                  "COMPLETE(c!)"
+                  "DISCARDED(d@/!)")))
 
 ;; ****************
-;;  States Colors!
+;;  STATES Colors!
 ;; ****************
 
 (setq org-todo-keyword-faces
-      '(("New Item"    . (:foreground "#005EB8" :weight bold))
-        ("In Progress" . (:foreground "#3BD1B7" :weight bold))
-        ("Researching" . (:foreground "#FB7306" :weight bold))
-        ("Backlogged"  . (:foreground "#A348C8" :weight bold))
-        ("In Review"   . (:foreground "#F1C232" :weight bold))
-        ("Blocked"     . (:foreground "#D13B55" :weight bold))
-        ("Warning"     . (:foreground "#FF1100" :weight bold))
-        ("Complete"    . (:foreground "#19D22A" :weight bold))
-        ("Discarded"   . (:foreground "#A7A7A7" :weight bold))))
+      '(("NEW ITEM"    . (:foreground "#005EB8" :weight bold))
+        ("IN PROGRESS" . (:foreground "#3BD1B7" :weight bold))
+        ("RESEARCHING" . (:foreground "#FB7306" :weight bold))
+        ("BACKLOGGED"  . (:foreground "#A348C8" :weight bold))
+        ("IN REVIEW"   . (:foreground "#F1C232" :weight bold))
+        ("BLOCKED"     . (:foreground "#D13B55" :weight bold))
+        ("WARNING"     . (:foreground "#FF1100" :weight bold))
+        ("COMPLETE"    . (:foreground "#19D22A" :weight bold))
+        ("DISCARDED"   . (:foreground "#A7A7A7" :weight bold))))
 
 ;; ************
 ;;  Item Tags!
